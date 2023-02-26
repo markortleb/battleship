8fd3a1f56f9e00525ab7f07e6ca7c54c603fc838
markortleb <markortleb@gmail.com>
2023-02-26 12:35:19 -0700

    working on player

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,19 @@
 import GameBoard from "./GameBoard";
 
-export default function Player() {
+export default function Player(type) {
     let _gameBoard = GameBoard();
+    let _type = type;
+
+    const getType = function () {
+        return _type;
+    };
+
+    const hasLost = function() {
+        return _gameBoard.getTotalLivingShips() > 0;
+    };
 
     return {
-
+        getType,
+        hasLost
     };
 }
