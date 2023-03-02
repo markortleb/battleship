5f07b0c911faef4e3d7de5217845ccf2734f139d
markortleb <markortleb@gmail.com>
2023-03-02 16:21:05 -0700

    use better icons

--- src/UI.js
+++ src/UI.js
@@ -2,6 +2,21 @@
 
 export default function UI() {
 
+    const _getUserBoard = (width, height) => {
+        let userBoardHtml = '<table class="user-board">';
+
+        for (let i = 0; i < height; i++) {
+            userBoardHtml += '<tr>'
+            for (let j = 0; j < width; j++) {
+                userBoardHtml += '<td></td>'
+            }
+            userBoardHtml += '</tr>'
+        }
+        userBoardHtml += '</table>'
+
+        return userBoardHtml;
+    };
+
     const skeleton = () => {
         return `
             <div class="title-area">
@@ -14,11 +29,29 @@ export default function UI() {
 
     const titleScreenInner = () => {
         return `
-        <span>Play Game!</span>
+            <span>Play Game!</span>
+        `;
+    }
+
+
+
+    const choosingScreenInner = (initialShipName) => {
+        return `
+        <span class="instructions">
+            Place your <span class="ship-name">${initialShipName}</span>.
+        </span>
+        ${_getUserBoard(10, 10)}
+        <div class="info-area">
+            <img src="src/img/arrow-left-bold-box-outline.svg" alt="">
+            <span>Use arrow keys to change orientation.</span>
+            <img src="src/img/arrow-right-bold-box-outline.svg" alt="">
+        </div>        
         `;
     }
 
     return {
-        skeleton
+        skeleton,
+        titleScreenInner,
+        choosingScreenInner
     }
 }
